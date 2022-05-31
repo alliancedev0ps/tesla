22be3f61f363ee34f680abaa6c0b2c03b9264db8
simondb <simondb@gmail.com>
2022-05-31 23:13:04 +0000

    modification adopted

--- deployd.sh
+++ deployd.sh
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
 deploy java application
+=======
+deply java applications
+deploy python apps 
+deploy in the dev env
+deployment5 master
+deployment4
+deploy to staging env
+echo good job
+>>>>>>> bafe6b9 (dynatrice added for APM)
