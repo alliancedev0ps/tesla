0b75c4b571e1492d655382b65521ef19f459cabe
simondb <simondb@gmail.com>
2022-05-31 23:07:18 +0000

    deleted files

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo welcome to landmark
-date
-echo apps are ready for deployment
-sleep 5
-echo deployment successful
--- deployd.sh
+++ deployd.sh
@@ -1,11 +1,15 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 deploy java application
 =======
+=======
+>>>>>>> a983624 (deployment to the staging environment)
 deply java applications
 deploy python apps 
 deploy in the dev env
 deployment5 master
 deployment4
+<<<<<<< HEAD
 deploy to staging env
 echo good job
 <<<<<<< HEAD
@@ -15,3 +19,6 @@ echo good job
 >>>>>>> bafe6b9 (dynatrice added for APM)
 =======
 >>>>>>> bafe6b9 (dynatrice added for APM)
+=======
+deploy to staging
+>>>>>>> a983624 (deployment to the staging environment)
